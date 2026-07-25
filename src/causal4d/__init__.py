d289bd21719dcd08aed6c6097cb9721c2a1c1e25
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-25 23:07:39 +0200

    Export grouped abduction APIs

--- src/causal4d/__init__.py
+++ src/causal4d/__init__.py
@@ -12,6 +12,21 @@ from causal4d.contracts import (
 )
 from causal4d.counterfactual import apply_counterfactual_operator
 from causal4d.evaluation import run_counterfactual_benchmark
+from causal4d.grouped_likelihood import (
+    GroupLikelihoodDiagnostics,
+    grouped_component_log_likelihoods,
+    posterior_weights_from_grouped_evidence,
+)
+from causal4d.identifiability import (
+    IdentifiabilityConfig,
+    InterventionIdentifiabilityResult,
+    assess_intervention_identifiability,
+    finite_response_sensitivity,
+)
+from causal4d.observation_evidence import (
+    GroupedObservationEvidence,
+    ObservationGroup,
+)
 from causal4d.rollout_bank import JointRolloutBank, SparseTrajectoryEvidence
 from causal4d.semantic_freshness import (
     SEMANTIC_TIMING_SCHEMA_VERSION,
@@ -26,8 +41,13 @@ __all__ = [
     "CounterfactualBenchmarkConfig",
     "CounterfactualQuery",
     "FactualIntervention",
+    "GroupLikelihoodDiagnostics",
+    "GroupedObservationEvidence",
+    "IdentifiabilityConfig",
+    "InterventionIdentifiabilityResult",
     "LatentContactConfig",
     "JointRolloutBank",
+    "ObservationGroup",
     "PhysicalPosterior",
     "SEMANTIC_TIMING_SCHEMA_VERSION",
     "SEMANTIC_TIMING_SCOPE",
@@ -39,7 +59,11 @@ __all__ = [
     "TwinBelief",
     "apply_counterfactual_operator",
     "apply_semantic_freshness_gate",
+    "assess_intervention_identifiability",
     "build_protocol",
+    "finite_response_sensitivity",
+    "grouped_component_log_likelihoods",
+    "posterior_weights_from_grouped_evidence",
     "run_counterfactual_benchmark",
     "run_latent_contact_benchmark",
 ]
